a59ba45a444b7700b64dc6df8f10d832e038557f
mrdcvlsc <mrdcvlsc@gmail.com>
2022-05-08 22:09:55 +0800

    added event handlers for transaction buttons

--- gui/js/transaction.js
+++ gui/js/transaction.js
@@ -1,6 +1,7 @@
 import { Table, RefreshTable } from './table.js';
 
 let InventoryTable = new Table(document.getElementById("inventory"),['Products','Class','Price','Quantity']);
+let BuyTable       = new Table(document.getElementById("chosen")   ,['Products','Class','Price','Quantity']);
 
 let SelectedItemname = document.getElementById('selected-item');
 
@@ -16,3 +17,63 @@ async function LoadInventory() {
 
 LoadInventory();
 setInterval(LoadInventory,800);
+
+// Filter Items
+let Filter = null;
+document.getElementById('filter').addEventListener('input', ()=> {
+  if(Filter) {
+    clearTimeout(Filter);
+  }
+  Filter = setTimeout(()=> {
+    // Filter Action
+  },1000);
+});
+
+// Buying an Item
+document.querySelector('.abtn').addEventListener('click', ()=> {
+  if(!InventoryTable.selected_tr) {
+    alert('Select an Item First in the Available Products Table');
+  }
+  else if(
+    document.getElementById('selected-quantity').value=='0' ||
+    document.getElementById('selected-quantity').value=='') {
+    alert('Input a Quantity First');
+  }
+  else {
+    // Add Item Action
+  }
+});
+
+// Removing an Item
+document.querySelector('.rbtn').addEventListener('click', ()=> {
+  if(!BuyTable.selected_tr) {
+    alert('Select an Item First to be Removed in the Products to be Sold Table');
+  }
+  else {
+    // Remove Item Action
+  }
+});
+
+// Calculate Change
+document.querySelector('.cbtn').addEventListener('click', ()=> {
+  if(
+    document.getElementById('payment').value=='0.0' ||
+    document.getElementById('payment').value=='0' ||
+    document.getElementById('payment').value==''
+  ) {
+    alert('Input a payment amount first');
+  }
+  else {
+    // Calculate Change Action
+  }
+});
+
+// Save Transactions
+document.querySelector('.sbtn').addEventListener('click', ()=> {
+  if(BuyTable.data) {
+    // Save Transaction Action
+  }
+  else {
+    alert('Fill the Product to be sold table first');
+  }
+});
